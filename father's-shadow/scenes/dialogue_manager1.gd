c5f830f8a06b015ee5a89f468cefe657d0a98dfd
Sahar8-coder <63341153+Sahar8-coder@users.noreply.github.com>
2026-04-21 02:00:37 +0300

    added cab and modified anims

--- father's-shadow/scenes/dialogue_manager1.gd
+++ father's-shadow/scenes/dialogue_manager1.gd
@@ -3,7 +3,7 @@ extends Control
 @onready var dialogue_panel = $DialoguePanel
 @onready var name_label = $DialoguePanel/NamePlate/NameLabel
 @onready var dialogue_text = $DialoguePanel/DialogueText
-@onready var portrait_root = $New_Portrait
+@onready var portrait = $DialoguePanel/Portrait
 
 @onready var timer_bar = $DialogueTimerBar
 @onready var choice_panel = $ChoicePanel
@@ -11,15 +11,15 @@ extends Control
 @onready var choice_2_text = $ChoicePanel/Background/"Choice2 [Y]"/AnswerOption2
 @onready var choice_3_text = $ChoicePanel/Background/"Choice3 [B]"/AnswerOption3
 
-@onready var end_anim_rect = $"../end_anim_rect"
-@onready var animation_player = $"../AnimationPlayer"
-
-@export var player: Node3D
-
 @onready var dialogue_state = $"../DialogueState"
 
 @onready var loyalty_state = $"../LoyaltyState"
 @onready var loyalty_value_label = $LoyaltyValueLabel
+
+@onready var end_anim_rect = $"../end_anim_rect"
+@onready var animation_player = $"../AnimationPlayer"
+
+@export var player: Node3D
 
 const LINE_TIME_LIMIT: float = 6.0
 const CHOICE_TIME_LIMIT: float = 8.0
@@ -31,8 +31,8 @@ var pending_node_id: String = ""
 var current_npc_id: String = ""
 
 func _ready() -> void:
-	#load_dialogue("res://dialogue/Leva.json")
-	load_dialogue("res://dialogue/Katerina.json")
+	end_anim_rect.visible = false
+	load_dialogue("res://dialogue/Leva.json")
 	dialogue_state.reset_dialogue_timer()
 	dialogue_state.start_dialogue_timer()
 	start_dialogue()
@@ -169,38 +169,44 @@ func show_current_node() -> void:
 
 
 func show_line_node(node: Dictionary) -> void:
-	set_ui_state(true, false)
+	dialogue_panel.show()
+	choice_panel.hide()
+	dialogue_state.start_window_timer(LINE_TIME_LIMIT)
 
 	name_label.text = str(node.get("name", ""))
 	dialogue_text.text = str(node.get("text", ""))
 
-	show_portrait_node(resolve_portrait_node(node))
-
+	var portrait_path = str(node.get("portrait", ""))
+	if portrait_path != "":
+		portrait.texture = load(portrait_path)
+	else:
+		portrait.texture = null
+	dialogue_state.start_window_timer(LINE_TIME_LIMIT)
+	
 	timer_bar.show()
 	timer_bar.value = 1.0
-	dialogue_state.start_window_timer(LINE_TIME_LIMIT)
+
 
 func show_choice_node(node: Dictionary) -> void:
-	set_ui_state(false, true)
-
-	show_portrait_node(resolve_portrait_node(node))
-
-	var options = node.get("options", [])
+	dialogue_panel.hide()
+	choice_panel.show()
 
 	choice_1_text.text = ""
 	choice_2_text.text = ""
 	choice_3_text.text = ""
 
+	var options = node.get("options", [])
 	if options.size() > 0:
 		choice_1_text.text = str(options[0].get("text", ""))
 	if options.size() > 1:
 		choice_2_text.text = str(options[1].get("text", ""))
 	if options.size() > 2:
 		choice_3_text.text = str(options[2].get("text", ""))
-
+	
+	dialogue_state.start_window_timer(CHOICE_TIME_LIMIT)
+	
 	timer_bar.show()
 	timer_bar.value = 1.0
-	dialogue_state.start_window_timer(CHOICE_TIME_LIMIT)
 
 
 func go_to_next_line() -> void:
@@ -258,8 +264,6 @@ func end_dialogue() -> void:
 	timer_bar.hide()
 	hide()
 
-
-
 func handle_window_timeout() -> void:
 	var node = nodes.get(current_node_id, {})
 	if node.is_empty():
@@ -289,7 +293,9 @@ func start_pause_before_next(next_id: String, pause_time: float) -> void:
 	pending_node_id = next_id
 	dialogue_state.start_between_window_timer(pause_time)
 
-	set_ui_state(false, false)
+	dialogue_panel.hide()
+	choice_panel.hide()
+	timer_bar.hide()
 	
 func update_loyalty_ui() -> void:
 	if current_npc_id == "":
@@ -297,75 +303,5 @@ func update_loyalty_ui() -> void:
 		return
 
 	var value = loyalty_state.get_loyalty(current_npc_id)
-	print("loyalty for ", current_npc_id, " = ", value)
+	# print("loyalty for ", current_npc_id, " = ", value)
 	loyalty_value_label.text = "" + str(value)
-
-func hide_all_portraits() -> void:
-	if portrait_root == null:
-		return
-
-	for character_node in portrait_root.get_children():
-		if character_node is CanvasItem:
-			character_node.hide()
-
-		for portrait_variant in character_node.get_children():
-			if portrait_variant is CanvasItem:
-				portrait_variant.hide()
-
-	if portrait_root is CanvasItem:
-		portrait_root.hide()
-
-func show_portrait_node(portrait_node_path: String) -> void:
-	hide_all_portraits()
-	if portrait_node_path == "" or portrait_root == null:
-		return
-	var portrait_node = portrait_root.get_node_or_null(portrait_node_path)
-	if portrait_node == null:
-		push_warning("Портрет не найден: " + portrait_node_path)
-		return
-	if portrait_node is CanvasItem:
-		portrait_node.show()
-	var parent = portrait_node.get_parent()
-	while parent != null:
-		if parent is CanvasItem:
-			parent.show()
-		if parent == portrait_root:
-			break
-		parent = parent.get_parent()
-
-func resolve_portrait_node(node: Dictionary) -> String:
-	var portrait_node_path = str(node.get("portrait_node", ""))
-	if portrait_node_path != "":
-		return portrait_node_path
-	var legacy_portrait = str(node.get("portrait", ""))
-	match legacy_portrait:
-		"res://art/Leva.png":
-			return "Leva/Leva_Base"
-		"res://art/Katerina.png":
-			return "Katerina/Katerina_Base"
-	return ""
-	
-func legacy_portrait_to_node_path(legacy_path: String) -> String:
-	match legacy_path:
-		"res://art/Katerina.png":
-			return "Katerina/Katerina_Base"
-		"res://art/Leva.png":
-			return "Leva/Leva_Base"
-	return ""
-
-func set_ui_state(show_dialogue: bool, show_choice: bool) -> void:
-	var ui_visible := show_dialogue or show_choice
-
-	dialogue_panel.visible = show_dialogue
-	choice_panel.visible = show_choice
-
-	if portrait_root is CanvasItem:
-		portrait_root.visible = ui_visible
-
-	if not ui_visible:
-		timer_bar.hide()
-		hide_all_portraits()
-
-	if not ui_visible:
-		timer_bar.hide()
-		hide_all_portraits()
