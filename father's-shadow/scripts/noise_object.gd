18de4adb8030b46bb3f8e44e37accca286aee5b3
unknown <ladejnovmatvej@mail.ru>
2026-04-22 17:08:53 +0300

    Ночной сегмент доделал все механики

--- father's-shadow/scripts/noise_object.gd
+++ father's-shadow/scripts/noise_object.gd
@@ -3,6 +3,15 @@ extends StaticBody3D
 @export var noise_amount: float = 15.0
 @onready var noise_area: Area3D = $NoiseArea
 
+var visible_for_player: bool = false
+
+func _ready() -> void:
+	add_to_group("noise_object")
+
+func set_visible_for_player(value: bool) -> void:
+	visible_for_player = value
+	visible = value
+
 func _on_noise_area_body_entered(body: Node3D) -> void:
 	if body.has_method("add_noise"):
 		body.add_noise(noise_amount)
