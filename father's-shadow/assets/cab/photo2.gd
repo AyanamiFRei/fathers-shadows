e3a9e7af49200c0bd8367e1d0a01622d5075fc6e
bihbiofficial-creator <bihbi.official@gmail.com>
2026-04-23 23:20:27 +0300

    sadfad123121111

--- father's-shadow/assets/cab/photo2.gd
+++ father's-shadow/assets/cab/photo2.gd
@@ -2,12 +2,10 @@ extends Area3D
 
 @onready var sprite: Sprite3D = get_parent()
 @onready var photo_label: Label = $"../../../../CanvasLayer3/PhotoLabel"
-@onready var photo_button: Button = get_tree().current_scene.get_node("CanvasLayer3/PhotoButton")
 
 @export var hover_color: Color = Color(1.25, 1.25, 1.25, 1.0)
 @export var normal_color: Color = Color(1.0, 1.0, 1.0, 1.0)
-@export var label_text: String = "Катерина. Давала лжесвидетельства на суде.
-Нужно еще раз с ней поговорить."
+@export var label_text: String = "Катерина. Давала лжесвидетельства на суде.\nНужно еще раз с ней поговорить."
 
 func _ready() -> void:
 	mouse_entered.connect(_on_mouse_entered)
@@ -21,13 +19,8 @@ func _ready() -> void:
 		push_error("PhotoLabel not found at CanvasLayer2/PhotoLabel")
 		return
 
-	if photo_button == null:
-		push_error("PhotoButton not found at CanvasLayer2/PhotoButton")
-		return
-
 	sprite.modulate = normal_color
 	photo_label.visible = false
-	photo_button.visible = false
 
 
 func _on_mouse_entered() -> void:
