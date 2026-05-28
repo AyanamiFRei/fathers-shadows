b0f7f7d7b631073ded35f30c7eb55fb1b4bc0573
iGore <glin4ak.igor@gmail.com>
2026-05-28 20:56:03 +0300

    add variable volume to noise_objects

--- father's-shadow/scripts/noise_object.gd
+++ father's-shadow/scripts/noise_object.gd
@@ -1,12 +1,16 @@
 extends StaticBody3D
 
 @export var noise_amount: float = 25.0
+@export var sound_volume_db: float = 0.0
 @onready var noise_area: Area3D = $NoiseArea
+@onready var noise_sound: AudioStreamPlayer = $NoiseSound 
 
 var visible_for_player: bool = false
 
 func _ready() -> void:
 	add_to_group("noise_object")
+	if noise_sound:
+		noise_sound.volume_db = sound_volume_db
 
 func set_visible_for_player(value: bool) -> void:
 	visible_for_player = value
